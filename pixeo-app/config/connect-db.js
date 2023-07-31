a9cb5fc2dd2f190daa0605775a206f8e4c70982d
Trique Nguyen <ngngoctridung123@gmail.com>
2023-07-31 12:19:42 -0700

    working on adding post

--- pixeo-app/config/connect-db.js
+++ pixeo-app/config/connect-db.js
@@ -1,9 +1,7 @@
 import { connect } from '@planetscale/database'
 
 const config = {
-  host: 'aws.connect.psdb.cloud',
-  username: '9c8pwhe5adosvxa23c77',
-  password: 'pscale_pw_pzUiVm38GtmHy5W6bcEV1jljMzTooYJJSj34vrJa0vB',
+
 }
 
 export default async function executeQuery({ query, values }) {
@@ -12,7 +10,7 @@ export default async function executeQuery({ query, values }) {
   try {
     const results = await conn.execute(query, values)
     return results
-  }  catch (error) {
+  } catch (error) {
     console.log(error)
   }
 }
