4fc9d3163cea4dca5c1104d3f667f147fd0b94fe
sid <hi@sid.fyi>
2023-07-31 21:21:15 -0700

    change input bg and border

--- pixeo-app/src/components/inputs/input.jsx
+++ pixeo-app/src/components/inputs/input.jsx
@@ -1,7 +1,7 @@
 export default function Input(props) {
   return (
     <input
-      className="bg-gray-100 w-full border focus:border-blue-500 focus:ring focus:outline-none text-sm rounded-lg p-2"
+      className="bg-gray-50 w-full border border-gray-300 focus:border-blue-500 focus:ring focus:outline-none text-sm rounded-lg p-2"
       {...props}
     />
   );
