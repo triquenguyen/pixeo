ba32e22100f27f300e2dff9f8fcee44687d85140
sid <hi@sid.fyi>
2023-08-08 11:32:18 -0700

    some fixes

--- pixeo-app/src/lib/user.js
+++ pixeo-app/src/lib/user.js
@@ -10,7 +10,7 @@ export const findByEmail = async (email) => {
 export const getAllPosts = async () => {
   return executeQuery({
     query: `SELECT post.id AS post_id, post.title, post.body, post.photo AS post_photo,
-                user.id AS user_id, user.firstname, user.lastname, user.email
+                user.id AS user_id, user.firstname, user.lastname, user.email, user.photo AS user_photo
                 FROM post
                 JOIN user ON post.user_id = user.id
                 ORDER BY post.id DESC;`,
