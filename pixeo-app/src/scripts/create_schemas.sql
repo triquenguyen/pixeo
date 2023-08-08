906aaa1df166091d0deab61402afaf9057584daf
sid <hi@sid.fyi>
2023-08-08 10:30:33 -0700

    update schema creation

--- pixeo-app/src/scripts/create_schemas.sql
+++ pixeo-app/src/scripts/create_schemas.sql
@@ -16,7 +16,7 @@ CREATE TABLE post (
     body TEXT NOT NULL,
     photo BLOB NOT NULL,
     user_id INT NOT NULL,
-    KEY user_id_idx (user_id),
+    KEY user_id_idx (user_id) REFERENCES user(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
 ) ENGINE=InnoDB;
 
@@ -25,8 +25,8 @@ CREATE TABLE follow (
     follower_id INT NOT NULL,
     following_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    KEY follower_id_idx (follower_id),
-    KEY following_id_idx (following_id)
+    KEY follower_id_idx (follower_id) REFERENCES user(id) ON DELETE CASCADE,
+    KEY following_id_idx (following_id) REFERENCES user(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 CREATE TABLE interest (
@@ -34,6 +34,6 @@ CREATE TABLE interest (
     post_id INT NOT NULL,
     user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    KEY post_id_idx (post_id),
-    KEY user_id_idx (user_id)
+    KEY post_id_idx (post_id) REFERENCES post(id) ON DELETE CASCADE,
+    KEY user_id_idx (user_id) REFERENCES user(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
