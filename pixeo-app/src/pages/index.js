3ca3b7af2565495098f7b23b0c61375fe23d3ec6
Trique Nguyen <ngngoctridung123@gmail.com>
2023-08-08 09:48:42 -0700

    tweaking the backgroun image

--- pixeo-app/src/pages/index.js
+++ pixeo-app/src/pages/index.js
@@ -7,19 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={`${inter.className} fixed inset-0 overflow-hidden`}>
-      <header className="flex items-center justify-between px-4 py-2">
-        <div className="flex items-center">
-          <Link href="/">
-            <Image alt="logo" height={37.5} src="/pixeo.svg" width={150} className="mr-2" />
-          </Link>
-        </div>
-
-        <div className="flex items-center">
-          <Link
-            className="mr-4 px-3 py-2 text-xl bg-[#000] rounded-md text-white hover:scale-105 active:95"
-            href={"/signup"}
-          >
-            Get Started Here
+      <Image src="/coverImage.png" width={1920} height={1080} className="hidden fixed xl:block min-h-screen z-[-1]" alt='bg' />
+      <div className="px-12 py-10">
+        <div className="flex items-center px-4 py-2">
+          <Link href="/" className="mr-auto" >
+            <Image alt="logo" height={37.5} src="/pixeo.svg" width={150} />
           </Link>
           <Link
             className="px-3 py-2 text-xl bg-[#000] rounded-md text-white hover:scale-105 active:95"
@@ -28,26 +20,19 @@ export default function Home() {
             Log In
           </Link>
         </div>
-      </header>
 
-      <body className="flex flex-col justify-center items-center min-h-screen relative">
-        {/* Background Image */}
-        <div className="absolute inset-0 z-0 ">
-          <Image
-            alt="coverImage"
-            layout="fill"
-            objectFit="cover"
-            src="/coverImage.png"
-            className=""
-          />
-        </div>
-
-        {/* Content */}
-        <div className="relative z-10 text-center px-3 py-2">
-          <h1 className="text-5xl font-bold text-white py-8">Welcome to Pixeo! A digital Artwork sharing Library.</h1>
+        <div className="flex flex-col justify-center items-center" style={{ overflowY: "hidden", height: "80vh" }}>
+          <h1 className="text-5xl font-bold text-white py-8">Welcome to Pixeo! A digital Artwork sharing Library</h1>
           <h1 className="text-3xl font-semibold text-white">Lets share your creativity!</h1>
+          <Link
+            className="mt-6 px-3 py-2 text-xl bg-[#000] rounded-md text-white hover:scale-105 active:95"
+            href={"/signup"}
+          >
+            Get Started Here
+          </Link>
         </div>
-      </body>
+      </div>
+
     </main>
   );
 }
