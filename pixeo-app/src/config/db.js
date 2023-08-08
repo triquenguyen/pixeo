ec267668fd42152c95064c0737a8fe1d0bddd02e
Taran-Chowdhury <taran.chowdhury@sjsu.edu>
2023-08-08 11:21:00 -0700

    config file

--- pixeo-app/src/config/db.js
+++ pixeo-app/src/config/db.js
@@ -1,6 +1,10 @@
 import { connect } from "@planetscale/database";
 
-const config = {};
+const config = {
+  username: 'azq0tpusnbtnfkute8n8',
+  host: 'aws.connect.psdb.cloud',
+  password: 'pscale_pw_Icnr53ux6lralkNNZn5X206oxX6hmucD3ywCOGcKROH'
+};
 
 export async function executeQuery({ query, values }) {
   const conn = await connect(config);
